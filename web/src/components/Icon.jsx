713e972d3a0fcecc0d5ca3dc872d13db38115da8
Nicolas <ncolondres@gmail.com>
2024-08-10 18:49:15 -0400

    idk what i did

---
+++ web/src/components/Icon.jsx
@@ -0,0 +1,10 @@
+function Icon({icon, type}){
+    
+    const path = `../../public/img/${type}/${icon}.png`
+
+
+    return (
+        <img src={path} alt={type}></img>
+    )
+}
+export default Icon
