a8cc3b56cf1fc97f2c5ca72adca32c44a2f05635
Nicolas <ncolondres@gmail.com>
2024-09-23 00:44:55 -0400

    Update images; player matches now dynamic

--- web/src/components/PlayerMatches.jsx
+++ web/src/components/PlayerMatches.jsx
@@ -12,17 +12,17 @@ function PlayerMatches({matches}) {
                     </div>
                     <ul id='matchStats'>
                         <li>KDA {match.kills}/{match.deaths}/{match.assists}</li>
-                        <li>Doubles {match.doubles}</li>
-                        <li>Triples {match.triples}</li>
-                        <li>Quadras {match.quadras}</li>
-                        <li>Pentas {match.pentas}</li>
-                        <li>Turrets {match.turrets}</li>
-                        <li>Inhibs {match.inhibs}</li>
-                        <li>Dragons {match.dragons}</li>
-                        <li>Rifts {match.rifts}</li>
-                        <li>Barons {match.barons}</li>
-                        <li>Vision {match.vision_score}</li>
-                        <li>Creeps {match.creep_score}</li>
+                        {match.doubles > 0 ? <li>Doubles {match.doubles}</li>: null}
+                        {match.triples > 0 ? <li>Triples {match.triples}</li>: null}
+                        {match.quadras > 0 ? <li>Quadras {match.quadras}</li>: null}
+                        {match.pentas > 0 ? <li>Pentas {match.pentas}</li>: null}
+                        {match.turrets > 0 ? <li>Turrets {match.turrets}</li>: null}
+                        {match.inhibs > 0 ? <li>Inhibs {match.inhibs}</li>: null}
+                        {match.dragons > 0 ? <li>Dragons {match.dragons}</li>: null}
+                        {match.rifts > 0 ? <li>Rifts {match.rifts}</li>: null}
+                        {match.barons > 0 ? <li>Barons {match.barons}</li>: null}
+                        {match.vision_score > 0 ? <li>Vision {match.vision_score}</li>: null}
+                        {match.creep_score > 0 ? <li>Creeps {match.creep_score}</li>: null}
                     </ul>
                 </div>
                 
