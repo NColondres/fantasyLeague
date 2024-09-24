de39419e7370ec767eb4c014d0f1f1d0c0cedf47
Nicolas <ncolondres@gmail.com>
2024-09-23 22:28:29 -0400

    more css work

--- web/src/components/PlayerMatches.jsx
+++ web/src/components/PlayerMatches.jsx
@@ -3,26 +3,27 @@ function PlayerMatches({matches}) {
     return (
         <>
             {matches.map(match =>
-                <div id='matchInfo' key={match.match_id} class={match.win ? 'winColors' : 'loseColors'}>
+                <div id='matchInfo' key={match.match_id} className={match.win ? 'winColors' : 'loseColors'}>
                     
                     <Icon id='championIcon' icon={match.champion} type='champion'/>
-                    <div>
-                        <div>{match.match_id}</div>
-                        <div class='noWrap'>{match.champion} {match.position}</div>
+                    <div id="first_box">
+                        <div className='noWrap'>{match.champion} {match.position}</div>
+                        <div id='match_id'>{match.match_id}</div>
                     </div>
+                    <div className='horizontal_separator'></div>
                     <ul id='matchStats'>
                         <li>KDA {match.kills}/{match.deaths}/{match.assists}</li>
-                        {match.doubles > 0 ? <li>Doubles {match.doubles}</li>: null}
-                        {match.triples > 0 ? <li>Triples {match.triples}</li>: null}
-                        {match.quadras > 0 ? <li>Quadras {match.quadras}</li>: null}
-                        {match.pentas > 0 ? <li>Pentas {match.pentas}</li>: null}
-                        {match.turrets > 0 ? <li>Turrets {match.turrets}</li>: null}
-                        {match.inhibs > 0 ? <li>Inhibs {match.inhibs}</li>: null}
-                        {match.dragons > 0 ? <li>Dragons {match.dragons}</li>: null}
-                        {match.rifts > 0 ? <li>Rifts {match.rifts}</li>: null}
-                        {match.barons > 0 ? <li>Barons {match.barons}</li>: null}
-                        {match.vision_score > 0 ? <li>Vision {match.vision_score}</li>: null}
-                        {match.creep_score > 0 ? <li>Creeps {match.creep_score}</li>: null}
+                        {match.doubles > 0 ? <li>Doubles {match.doubles}</li> : null}
+                        {match.triples > 0 ? <li>Triples {match.triples}</li> : null}
+                        {match.quadras > 0 ? <li>Quadras {match.quadras}</li> : null}
+                        {match.pentas > 0 ? <li>Pentas {match.pentas}</li> : null}
+                        {match.turrets > 0 ? <li>Turrets {match.turrets}</li> : null}
+                        {match.inhibs > 0 ? <li>Inhibs {match.inhibs}</li> : null}
+                        {match.dragons > 0 ? <li>Dragons {match.dragons}</li> : null}
+                        {match.rifts > 0 ? <li>Rifts {match.rifts}</li> : null}
+                        {match.barons > 0 ? <li>Barons {match.barons}</li> : null}
+                        {match.vision_score > 0 ? <li>Vision {match.vision_score}</li> : null}
+                        {match.creep_score > 0 ? <li>Creeps {match.creep_score}</li> : null}
                     </ul>
                 </div>
                 
